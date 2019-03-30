fd30c1fe55cabf7106f1e3e190388a47cb8f9037
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-03-30 11:38:12 +0530

    sum utils

---
+++ app.js
@@ -0,0 +1,6 @@
+const sum = require('./utils/sum');
+
+const a = 42;
+const b = 23;
+
+console.log(sum(a, b));
---
+++ utils/sum.test.js
@@ -0,0 +1,5 @@
+const sum = require('./sum');
+
+test('7 + 3 should return 10', () => {
+  expect(sum(7, 3)).toBe(10);
+})
